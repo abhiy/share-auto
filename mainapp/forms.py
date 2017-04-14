be7ebbf664da24d66f5362f4fa5ff34b4794c1d7
abhiy <abhiy@iitk.ac.in>
2017-04-15 02:42:09 +0530

    Fixed redirection

--- mainapp/forms.py
+++ mainapp/forms.py
@@ -1,4 +1,7 @@
 from django import forms
 
 class phoneForm(forms.Form):
-	phoneNumber = forms.CharField(label='Enter your phone number:')
+	phoneNumber = forms.CharField(label='Enter your phone number:', max_length=100)
+
+class NameForm(forms.Form):
+    your_name = forms.CharField(label='Your name', max_length=100)
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .forms import phoneForm 
+from .forms import NameForm 
 
-# Create your views here.
+# Create your views here
 def startup(request):
-	return render(request, "startup.html")
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = phoneForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            number = form.cleaned_data['phoneNumber']
+            context = {'number' : number}
+            return render(request, "resp.html", context)
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = phoneForm()
+
+    return render(request, 'startup.html', {'form': form})
 
 def listings(request, number):
 	context = {'number': number}
