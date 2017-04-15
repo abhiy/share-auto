b60cb81e7a717a8d8de0d42a8ca4b15a9217cea4
abhiy <abhiy@iitk.ac.in>
2017-04-16 04:02:33 +0530

    Changed settings

--- shareAuto/settings.py
+++ shareAuto/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'shareAuto',
         'USER': 'user',
-        'PASSWORD': 'passpass',
+        'PASSWORD': 'password',
         'HOST': 'localhost',
         'PORT': '',
     }
